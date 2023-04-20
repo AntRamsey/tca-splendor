d86bcf93e44d4670fb0edb505291a93b0b5f2586
AntRamsey <antwoneramsey@gmail.com>
2023-04-20 00:16:48 -0500

    Done with screens/initial-navigation
    nav(-2) trickery

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,9 +1,23 @@
-
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom'
 
 export const Play = () => {
+    const nav = useNavigate();
+
     return (
-        <h2>
-            Play
-        </h2>
+        <>
+            <h2>
+                Play
+            </h2>
+            <p>
+                Some data collection stuff...
+            </p>
+            <Button 
+                variant="outline-primary"
+                onClick={() => nav(-2)}
+            >
+                Done
+            </Button>
+        </>
     )
 };
--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,9 +1,23 @@
-
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom'
 
 export const Setup = () => {
+    const nav = useNavigate();
+
     return (
-        <h2>
-            Setup
-        </h2>
+        <>
+            <h2>
+                Setup
+            </h2>
+            <p>
+                Some setup stuff...
+            </p>
+            <Button 
+                variant="outline-primary"
+                onClick={() => nav("/play")}
+            >
+                Start Game
+            </Button>
+        </>
     )
 };
