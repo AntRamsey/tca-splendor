6a3a7b5a1c436899ef54804ecdceb592da9d7250
AntRamsey <antwoneramsey@gmail.com>
2023-04-20 00:07:54 -0500

    screens/initial-navigation kind of working
    still needs buttons...

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Home } from './Home';
+import { Setup } from './Setup';
+import { Play } from './Play';
+
+import {
+  HashRouter
+  , Routes
+  , Route
+} from 'react-router-dom'
 
 
 function App() {
@@ -15,7 +22,13 @@ function App() {
         Companion App
       </h2>
       <hr />
-      <Home />
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/setup' element={<Setup />} />
+          <Route path='/play' element={<Play />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
